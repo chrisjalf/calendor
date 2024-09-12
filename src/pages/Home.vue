0aa18a764ba91f0ff46e830ddf105f049f3c9753
Chris James <xcastroj97@gmail.com>
2024-09-12 23:31:46 +0800

    .

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,5 +1,5 @@
 <script lang="ts">
-import { defineComponent, Ref, ref } from "vue";
+import { defineComponent, Reactive, reactive } from "vue";
 import FullCalendar from "@fullcalendar/vue3";
 import dayGridPlugin from "@fullcalendar/daygrid";
 import timeGridPlugin from "@fullcalendar/timegrid";
@@ -9,12 +9,16 @@ import { CalendarOptions } from "@fullcalendar/core";
 export default defineComponent({
   components: { FullCalendar },
   setup() {
-    const calendarOptions: Ref<CalendarOptions> = ref({
+    const events = reactive([
+      { title: "event 1", start: "2024-09-01" },
+      { title: "event 2", start: "2024-09-02" },
+    ]);
+    const calendarOptions: Reactive<CalendarOptions> = reactive({
       plugins: [dayGridPlugin, timeGridPlugin, interactionPlugin],
       headerToolbar: {
-        left: "prev,next",
+        left: "prev today next",
         center: "title",
-        right: "today dayGridMonth,timeGridWeek,timeGridDay",
+        right: "dayGridMonth,timeGridWeek,timeGridDay",
       },
       buttonText: {
         today: "Today",
@@ -23,14 +27,18 @@ export default defineComponent({
         day: "Day",
       },
       initialView: "dayGridMonth",
-      events: [
-        { title: "event 1", start: "2024-09-01" },
-        { title: "event 2", start: "2024-09-02" },
-      ],
+      events: events,
       dayMaxEvents: true,
     });
 
-    return { calendarOptions };
+    function todo() {
+      events.push({
+        title: "event 3",
+        start: "2024-09-12",
+      });
+    }
+
+    return { calendarOptions, todo };
   },
 });
 </script>
@@ -43,43 +51,17 @@ export default defineComponent({
       </div>
       <!-- Placeholder -->
       <div class="col-lg-4">
-        <form>
-          <div class="mb-3">
-            <label for="exampleInputEmail1" class="form-label"
-              >Email address</label
-            >
-            <input
-              type="email"
-              class="form-control"
-              id="exampleInputEmail1"
-              aria-describedby="emailHelp"
-            />
-            <div id="emailHelp" class="form-text">
-              We'll never share your email with anyone else.
-            </div>
-          </div>
-          <div class="mb-3">
-            <label for="exampleInputPassword1" class="form-label"
-              >Password</label
-            >
-            <input
-              type="password"
-              class="form-control"
-              id="exampleInputPassword1"
-            />
-          </div>
-          <div class="mb-3 form-check">
-            <input
-              type="checkbox"
-              class="form-check-input"
-              id="exampleCheck1"
-            />
-            <label class="form-check-label" for="exampleCheck1"
-              >Check me out</label
-            >
-          </div>
-          <button type="submit" class="btn btn-primary">Submit</button>
-        </form>
+        <div class="mb-3">
+          <label class="form-label">Name</label>
+          <input type="email" class="form-control" />
+        </div>
+        <div class="mb-3">
+          <label class="form-label">Description</label>
+          <textarea class="form-control"></textarea>
+        </div>
+        <button type="submit" class="btn btn-primary" @click="todo">
+          Submit
+        </button>
       </div>
     </div>
   </div>
