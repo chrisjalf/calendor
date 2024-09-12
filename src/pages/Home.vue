cd29bb19f243cf0cdef5fad1540aa3fa38cd0dbd
Chris James <xcastroj97@gmail.com>
2024-09-12 22:23:33 +0800

    include time grid and set dayMaxEvents=true

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,19 +1,33 @@
 <script lang="ts">
-import { defineComponent, ref } from "vue";
+import { defineComponent, Ref, ref } from "vue";
 import FullCalendar from "@fullcalendar/vue3";
 import dayGridPlugin from "@fullcalendar/daygrid";
+import timeGridPlugin from "@fullcalendar/timegrid";
 import interactionPlugin from "@fullcalendar/interaction";
+import { CalendarOptions } from "@fullcalendar/core";
 
 export default defineComponent({
   components: { FullCalendar },
   setup() {
-    const calendarOptions = ref({
-      plugins: [dayGridPlugin, interactionPlugin],
+    const calendarOptions: Ref<CalendarOptions> = ref({
+      plugins: [dayGridPlugin, timeGridPlugin, interactionPlugin],
+      headerToolbar: {
+        left: "prev,next",
+        center: "title",
+        right: "today dayGridMonth,timeGridWeek,timeGridDay",
+      },
+      buttonText: {
+        today: "Today",
+        month: "Month",
+        week: "Week",
+        day: "Day",
+      },
       initialView: "dayGridMonth",
       events: [
-        { title: "event 1", date: "2024-09-01" },
-        { title: "event 2", date: "2024-09-02" },
+        { title: "event 1", start: "2024-09-01" },
+        { title: "event 2", start: "2024-09-02" },
       ],
+      dayMaxEvents: true,
     });
 
     return { calendarOptions };
