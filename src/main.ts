1b1cb699997092bd7c93d2df1f2dda400c02d463
Chris James <xcastroj97@gmail.com>
2024-09-13 00:24:27 +0800

    vue router

--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,10 @@ import { createApp } from "vue";
 import "bootstrap/dist/css/bootstrap.css";
 
 import App from "./App.vue";
+import router from "./router.ts";
 
-createApp(App).mount("#app");
+const app = createApp(App);
+
+app.use(router);
+
+app.mount("#app");
