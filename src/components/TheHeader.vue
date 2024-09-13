55bb608830d6e56a963e26454986e92e49671e77
Chris James <xcastroj97@gmail.com>
2024-09-13 15:56:25 +0800

    .

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -11,16 +11,18 @@ export default defineComponent({
     <header
       class="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom"
     >
-      <a
-        href="/"
+      <router-link
+        to="/"
         class="d-flex align-items-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none"
       >
         <span class="fs-4">Calendor</span>
-      </a>
+      </router-link>
 
       <ul class="nav nav-pills">
         <li class="nav-item">
-          <a href="/" class="nav-link active" aria-current="page">Home</a>
+          <router-link to="/" class="nav-link active" aria-current="page">
+            Home
+          </router-link>
         </li>
       </ul>
     </header>
--- src/router.ts
+++ src/router.ts
@@ -6,7 +6,7 @@ const NotFound = () => import("./pages/NotFound.vue");
 const router = createRouter({
   history: createWebHistory(),
   routes: [
-    { path: "/", component: Home },
+    { path: "/", component: Home, alias: "/events" },
     { path: "/:notFound(.*)", component: NotFound },
   ],
 });
